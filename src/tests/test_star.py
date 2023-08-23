56fa58e2159b3dae7b22645fe284038503f02743
Michael Bailey <michael.t.bailey@gmail.com>
2023-08-24 09:33:54 +1000

    Lint fixes on test_star.py

--- src/tests/test_star.py
+++ src/tests/test_star.py
@@ -1,67 +1,72 @@
+''' test_star.py:  Unit Tests for star generation '''
+
 # from os import popen
 # from _pytest.python_api import ApproxMapping
-import pytest
+
 import os
 import sys
+import pytest
+
+from src.star import Star
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from src.star import Star
 
-@pytest.fixture(scope='session')
-def star_Collection():
-    starCollection = []
+@pytest.fixture(name='star_collection', scope='session')
+def fixture_star_collection():
+    ''' A fixture to generate a collection of star objects for testing '''
+    star_collection = []
     for i in range(0, 10001):
-        starName = 'Test' + str(i)
-        thisStar = Star()
-        thisStar.starName = starName
-        thisStar.genStar(0, False, False)
-        data = {'Star Age': thisStar.starAge}
-        data['Star Class'] =  thisStar.starClass
-        data['Star Type'] = thisStar.starType
-        data['Star SubType'] = thisStar.starSubType
-        starCollection.append(data)
+        star_name = 'Test' + str(i)
+        this_star = Star()
+        this_star.star_name = star_name
+        this_star.genStar(0, False, False)
+        data = {'Star Age': this_star.starAge}
+        data['Star Class'] =  this_star.starClass
+        data['Star Type'] = this_star.starType
+        data['Star SubType'] = this_star.starSubType
+        star_collection.append(data)
+    return star_collection
 
-    return starCollection
+# Test star age
 
-''' Test star age '''
-def test_starAge(star_Collection):
-
+def test_star_age(star_collection):
+    ''' Check the star age is within bounds '''
     errors = []
-    assert star_Collection is not None
-    for data_point in star_Collection: 
-            
-        # replace assertions by conditions because we are
-        # looping through numerous data points and dont want to stop
-        # on the first failure
-        if not (data_point["Star Age"] <= 12.0 or data_point["Star Age"] == 0): 
+    assert star_collection is not None
+    for data_point in star_collection:
+
+        # replace assertions by conditions because we are  looping through
+        # numerous data points and dont want to stop on the first failure
+
+        if not (data_point["Star Age"] <= 12.0 or data_point["Star Age"] == 0):
 
             # Format the error message
-            errorMsg = 'Invalid age: ' + str(data_point["Star Age"]) \
+            error_msg = 'Invalid age: ' + str(data_point["Star Age"]) \
                 + ' ' + data_point['Star Type'] \
                 + str(data_point['Star SubType']) + ' ' \
                 + data_point['Star Class'] + ')'
-            errors.append(errorMsg)
-    
+            errors.append(error_msg)
+
     # assert no error message has been registered, else print messages
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
-''' Test star Class VI restrictions '''
-def test_starClassVI(star_Collection):
-
+# Test star Class VI restrictions
+def test_star_class_vi(star_collection):
+    ''' Check that Class VI stars have correct type and subtype '''
     errors = []
-    assert star_Collection is not None
-    for data_point in star_Collection:    
+    assert star_collection is not None
+    for data_point in star_collection:
 
         # replace assertions by conditions as above
         if (data_point['Star Class'] == 'VI') \
-            and data_point['Star Type'] in ('A', 'F'):
-                
-                # Format the error message
-                errorMsg = 'Invalid type for Class VI star: ' \
-                + (data_point["Star Type"])
-                errors.append(errorMsg)
-    
+        and data_point['Star Type'] in ('A', 'F'):
+
+            # Format the error message
+            error_msg = 'Invalid type for Class VI star: ' \
+            + (data_point["Star Type"])
+            errors.append(error_msg)
+
     # assert no error message has been registered, else print messages
-    assert not errors, "errors occured:\n{}".format("\n".join(errors))            
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
